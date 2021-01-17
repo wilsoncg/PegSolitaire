5a80f9d25bad2219bffdd2839f66448d7b495a2f
Craig Wilson <craig@wilsoncg.net>
2021-01-17 21:08:30 +0000

    spit out graphviz in graph.dot file

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using PegSolitaire;
 using QuickGraph;
+using QuickGraph.Graphviz;
 using QuickGraph.Graphviz.Dot;
 
 namespace TestApp
@@ -17,23 +19,22 @@ namespace TestApp
             PegBoard targetState = CreateTargetState();
             Console.WriteLine(string.Format("Initial state: {0}", initialState));
             Console.WriteLine(string.Format("Target state: {0}", targetState));
-            var solver = new PegSolitaireSolver(initialState, targetState);
-            //var path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+
+            var solver = new PegSolitaireSolver(initialState, targetState);            
             Console.WriteLine("Starting graph generation...");
             var graph = solver.GenerateGraph();
-            //var dfs = solver.DepthFirstSearch(graph);
-            //var edges = graph.Edges.Select(x => x.Source + "->" + x.Target);
-            //foreach (var state in dfs.Keys)
-            //{
-            //    Console.WriteLine(state);
-            //}
-            //Console.WriteLine();
+            Console.WriteLine("{0} possible moves generated (dead ends included)", graph.Vertices.Count());
+            
             Console.WriteLine(string.Format("Contains solution: {0}", solver.ContainsSolution(graph)));
             var shortestPath = solver.FindShortestPath(graph);
             PrintPath(shortestPath);
-            //var graphviz = new QuickGraph.Graphviz.GraphvizAlgorithm<PegBoard, Edge<PegBoard>>(graph, path+"\\graph.dot", GraphvizImageType.Png);
-            //graphviz.Generate();
-            Console.WriteLine(graph.Vertices.Count());
+
+            var graphviz = new GraphvizAlgorithm<PegBoard, Edge<PegBoard>>(graph);
+            graphviz.FormatVertex += (sender, eventArgs) =>
+            {
+                eventArgs.VertexFormatter.Label = eventArgs.Vertex.ToString();
+            };
+            graphviz.Generate(new FileDotEngine(), "graph.dot");
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Press a key to exit.");
@@ -42,33 +43,34 @@ namespace TestApp
             Console.WriteLine();
             Console.WriteLine("Exiting...");
             Environment.Exit(1);
-
         }
 
         private static PegBoard CreateTargetState()
         {
-            return new PegBoard()
-                       {
-                           Pegs =
-                               {
-                                   new Peg(PegState.Blue), new Peg(PegState.Blue), new Peg(PegState.Blue), new Peg(PegState.Blue), 
-                                   new Peg(PegState.Empty), new Peg(PegState.Empty),
-                                   new Peg(PegState.Red), new Peg(PegState.Red), new Peg(PegState.Red), new Peg(PegState.Red)
-                               }
-                       };
+            return 
+                new PegBoard()
+                {
+                    Pegs =
+                    {
+                        new Peg(PegState.Blue), new Peg(PegState.Blue), new Peg(PegState.Blue), new Peg(PegState.Blue), 
+                        new Peg(PegState.Empty),
+                        new Peg(PegState.Red), new Peg(PegState.Red), new Peg(PegState.Red), new Peg(PegState.Red)
+                    }
+                };
         }
 
         private static PegBoard CreateInitialState()
         {
-            return new PegBoard()
-                       {
-                           Pegs =
-                               {
-                                   new Peg(PegState.Red), new Peg(PegState.Red), new Peg(PegState.Red), new Peg(PegState.Red), 
-                                   new Peg(PegState.Empty), new Peg(PegState.Empty),
-                                   new Peg(PegState.Blue), new Peg(PegState.Blue), new Peg(PegState.Blue), new Peg(PegState.Blue)
-                               }
-                       };
+            return 
+                new PegBoard()
+                {
+                    Pegs =
+                    {
+                        new Peg(PegState.Red), new Peg(PegState.Red), new Peg(PegState.Red), new Peg(PegState.Red), 
+                        new Peg(PegState.Empty),
+                        new Peg(PegState.Blue), new Peg(PegState.Blue), new Peg(PegState.Blue), new Peg(PegState.Blue)
+                    }
+                };
         }
 
         private static void PrintPath(List<Edge<PegBoard>> path)
@@ -77,6 +79,16 @@ namespace TestApp
             {
                 Console.WriteLine("[{0}->{1}]", edge.Source, edge.Target);
             }
+        }
+    }
+
+    public class FileDotEngine : IDotEngine
+    {
+        public string Run(GraphvizImageType imageType, string dot, string outputFileName)
+        {
+            var output = outputFileName;
+            File.WriteAllText(output, dot);
+            return output;
         }
     }
 }
